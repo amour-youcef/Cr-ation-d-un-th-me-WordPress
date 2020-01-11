1ff2c3d7b3ca4d066a2b1f63425062bdbfce0522
amour-youcef <amour-youcef@hotmail.com>
2020-01-11 08:49:00 +0100

    maj

---
+++ wordpress/wp-content/themes/monsupertheme/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php bloginfo('name'); ?> - <?php bloginfo('description')?> </title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>  ">
+</head>
+<body>
+    <h1>Les acteurs</h1>
+</body>
+</html>
